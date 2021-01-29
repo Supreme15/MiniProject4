30c4472249005b04a27b589ced56e9a7ae47ede8
Darrel Supreme <darrel.supreme@patelhs.org>
2021-01-29 17:25:22 +0000

    Made first method

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 class Main {
-  public static void main(String[] args) 
-  {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner.next(System.in);
+
+    System.out.println("Hello, want to hear a joke?");
     
   }
 
-  //
+  //Hello method
+  static String pun(String name)
+  {
+    String joke = "What type of sandals do frogs wear?";
+    return joke;
+  }
 }
